fd70b0853204b244a0d4e3103890ae4bcc85d878
May Yi Aung <may@aiems.ae>
2024-07-28 13:21:39 +0400

    Initial Commit

---
+++ Restaurant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant
+//
+//  Created by May Yi on 28/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
